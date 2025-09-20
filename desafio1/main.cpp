1ecb6159ad904b5a4f7e3697a354ae0d37300860
Santiago Gallon Restrepo <santiago.gallon1@udea.edu.co>
2025-09-19 21:55:41 -0500

    Limpieza: elimino archivos de build y caché del repocitorio, agrego .gitignore

---
+++ desafio1/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <QThread>
+using namespace std;
+
+int main()
+{
+     int situacion=0;
+    if (lectComplet)
+    {
+        situacion =1;
+    }
+    else
+    {
+        situacion=0;
+    }
+
+    switch(situacion)
+{
+    }
+
+}
