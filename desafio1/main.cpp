fce32223bf538c9f09eef2f328f9a45110dc194d
Santiago Gallon Restrepo <santiago.gallon1@udea.edu.co>
2025-09-22 19:16:51 -0500

    inicie el  codigo de descomprecion LZ78 usando punteros dinámicos

--- desafio1/main.cpp
+++ desafio1/main.cpp
@@ -1,21 +1,39 @@
 #include <iostream>
-#include <QThread>
+#include <fstream>
+#include <cstring>
+#include <cstdlib>
 using namespace std;
+char** addEntry(char  **diccionario, int&tamano,const char *newEntry);
+char* LZ78Decode(const char *ingresso);
 
 int main()
 {
-     int situacion=0;
-    if (lectComplet)
-    {
-        situacion =1;
-    }
-    else
-    {
-        situacion=0;
-    }
 
-    switch(situacion)
+}
+char** addEntry(char  **diccionario, int&tamano,const char *newEntry)
 {
+    char ** temporal = new char* [tamano +1];
+    for (int i=0; i<tamano;i++) temporal [i]=diccionario[i];
+    temporal[tamano] =new char [strlen (newEntry) + 1];
+    strcpy(temporal [tamano],newEntry);
+    delete []diccionario;
+    tamano++;
+    return temporal;
+}
+char* LZ78Decode(const char *innt)
+{
+    char **diccionario = nullptr;
+    int tamDiccionario=0;
+    char *resul = new char[1]
+        resul[0]='\0';
+    int resultado=0;
+    const char *p =innt;
+    while(*P)
+    {
+        if (*p =='(')
+        {
+            p++;
+char numbuf
+        }
     }
-
 }
